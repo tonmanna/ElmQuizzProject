623a2188fb20ac20cd7f69f68f7bc400a3e214b2
Worawut Boontan <worawut@theiconweb.com>
2019-09-30 03:09:13 +0700

    Subscription + Send

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { Elm } from "./src/components/quiz.elm";
-
-Elm.SimpleQuizz.init({
-  node: document.querySelector("main")
+var app = Elm.SimpleQuizz.init({node: document.getElementById("app"), flags : "0.0"});
+app.ports.sendToJS.send("1.0.0")
+app.ports.subFromJS.subscribe(function(options) {
+  console.log(options);  
 });
--- src/components/quiz.elm
+++ src/components/quiz.elm
@@ -1,29 +1,39 @@
-module SimpleQuizz exposing (main)
+port module SimpleQuizz exposing (..)
 
 import Browser
 import Html exposing (Html, div, text, label, input, textarea, span, h1, p, a)
-import Html.Attributes exposing (attribute, class, placeholder, type_, for, rows, style, href, hidden)
+import Html.Attributes exposing (attribute, class, placeholder, type_, for, rows, style, href, hidden, id)
 import Html.Events exposing (onClick)
+
 
 import List exposing (..)
 
+
+type alias QuestionListModel = { questions : List Question, currentQuestion: Int, hiddenQuestion: Bool, version: String}
 type alias Question = {  no: Int, title: String, answer: String, mermaid: String, code: String }
-initQuestion: Question
-initQuestion = { no = 0, title = "FINISH", answer = "", mermaid = "", code="" }
+type Message = LetPlay | ClickNext | ClickBack | FromJS String | SendToJS
 
-
-type alias Model =  { questions : List Question, currentQuestion: Int, hiddenQuestion: Bool}
-initQuizModel: Model
-initQuizModel = 
-  { questions = 
+initialModel : QuestionListModel
+initialModel ={ 
+    questions = 
     [ { no = 1, title = "The example uses a WHERE clause to show the only person team leader.", answer = "111", mermaid = "", code = "" }
     , { no = 2, title = "The example uses a WHERE clause to show the only person team leader.", answer = "222 ", mermaid = "", code = "" }
     ]
     , currentQuestion = 1
     , hiddenQuestion = True
+    , version = "0.0"
   }
 
-type Message = LetPlay | ClickNext | ClickBack
+init: String -> (QuestionListModel, Cmd Message)
+init flags = (initialModel, Cmd.none)
+
+initQuestion: Question
+initQuestion = { no = 0, title = "FINISH", answer = "", mermaid = "", code="" }
+
+subscriptions : QuestionListModel -> Sub Message
+subscriptions model = subFromJS FromJS
+port subFromJS : (String -> msg) -> Sub msg
+port sendToJS : String -> Cmd msg
 
 viewQuestion : Question -> Html Message
 viewQuestion question =
@@ -35,17 +45,18 @@ viewQuestion question =
                 [text question.answer]
             , div [ class "invalid-feedback" ]
                 [ text "Please enter your valid SQL." ]
+            , div [ class "mermaid", id "mermaidChart0" ] [text ""]
             ]
         ]
 
-viewNextBackQuestion : Model -> Html Message
+viewNextBackQuestion : QuestionListModel -> Html Message
 viewNextBackQuestion model =
     div [ class "row"] 
         [ span [ class "btn btn-info", onClick ClickNext] [text "Next"]
         , span [ class "btn btn-warning", onClick ClickBack, style "margin-left" "5px"] [text "Back"]
         ]
 
-view: Model -> Html Message
+view: QuestionListModel -> Html Message
 view model = 
     let 
       currentQuestions = List.filter (\x -> x.no == model.currentQuestion) model.questions
@@ -60,6 +71,8 @@ view model =
             , p []
                 [ text "“You can’t stop the future. You can’t rewind the past.The only way to learn the secret s to press play.”" ]
             , p []
+                [ label [ class "badge badge-secondary"] [ text ("Version:" ++ model.version)]]
+            , p []
                 [ span [ class "btn btn-primary btn-lg", onClick LetPlay ]
                     [ text "Let's Play »" ]
                 ]
@@ -72,28 +85,35 @@ view model =
             , p []
                 [ text "“You can’t stop the future. You can’t rewind the past.The only way to learn the secret s to press play.”" ]
             , p []
-                [ span [ class "btn btn-primary btn-lg", onClick LetPlay ]
-                    [ text "Submit exam awnser" ]
+                [ span [ class "btn btn-primary btn-lg", onClick SendToJS ]
+                    [ text "Submit exam answer" ]
                 ]    
             ]
         ]
-        
-update : Message -> Model -> Model
+
+update : Message -> QuestionListModel ->(QuestionListModel, Cmd Message)
 update msg model =
     case msg of
         ClickNext ->          
-          { model | currentQuestion = model.currentQuestion + 1 }
+          ({ model | currentQuestion = model.currentQuestion + 1 }, Cmd.none)
         ClickBack ->
           if model.currentQuestion > 1 then
-            { model | currentQuestion = model.currentQuestion - 1 }
+            ({ model | currentQuestion = model.currentQuestion - 1 }, Cmd.none)
           else
-            { model | hiddenQuestion = True }
+            ({ model | hiddenQuestion = True }, Cmd.none)
         LetPlay ->
-          { model | hiddenQuestion = False }
+          ({ model | hiddenQuestion = False }, Cmd.none)
+        SendToJS ->
+          (model , sendToJS "Empty")
+        FromJS value ->
+          ({ model | version = value }, Cmd.none)
+
+
 
 main =
-    Browser.sandbox
-        { init = initQuizModel
+    Browser.element
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
