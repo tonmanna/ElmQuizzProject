ed1e6b709b2ecd97a83beafbfacbf6b735b8329b
Worawut Boontan <worawut@theiconweb.com>
2022-11-18 22:15:19 +0700

    Fix Master

--- src/js/controllers/ports_subscribe/code_heighlight.js
+++ src/js/controllers/ports_subscribe/code_heighlight.js
@@ -1,4 +1,4 @@
-import Prism from "prismjs";
+import * as Prism from "prismjs";
 import MarkdownIt from "markdown-it";
 const md = new MarkdownIt();
 export default (model) => {
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import { plugin } from "vite-plugin-elm";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
-
+import legacy from "@vitejs/plugin-legacy";
 export default defineConfig({
-  plugins: [plugin(), monacoEditorPlugin({})],
+  plugins: [
+    plugin(),
+    monacoEditorPlugin({}),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
   build: {
     outDir: "./dist",
     rollupOptions: {
