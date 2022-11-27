a82d114d1217664035843118ad3c7f16b3570266
Worawut Boontan <worawut@theiconweb.com>
2022-11-27 15:16:43 +0700

    Prevent Data Error

--- src/js/controllers/ports_subscribe/monaco_get_text.js
+++ src/js/controllers/ports_subscribe/monaco_get_text.js
@@ -2,10 +2,9 @@ import "monaco-editor/esm/vs/editor/editor.all.js";
 import "monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js";
 
 function waitUntilEditorDefine(model, app) {
-  console.log("model: ", model);
   const element = "container" + model.questionNumber;
   const editor = document.getElementById(element);
-  if (!editor) {
+  if (!editor || !window.currentEditor) {
     setTimeout(() => {
       waitUntilEditorDefine(model, app);
     }, 100);
