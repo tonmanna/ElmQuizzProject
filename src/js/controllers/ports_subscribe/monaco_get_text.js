1886086509d5061e27f277e661703ee1b99deafc
Worawut Boontan <worawut@theiconweb.com>
2022-11-27 15:06:20 +0700

    Update Wait Element

--- src/js/controllers/ports_subscribe/monaco_get_text.js
+++ src/js/controllers/ports_subscribe/monaco_get_text.js
@@ -1,12 +1,20 @@
 import "monaco-editor/esm/vs/editor/editor.all.js";
 import "monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js";
 
-export default (model, app) => {
-  if (window.currentEditor) {
+function waitUntilEditorDefine(model, app) {
+  console.log("model: ", model);
+  const element = "container" + model.questionNumber;
+  const editor = document.getElementById(element);
+  if (!editor) {
+    setTimeout(() => {
+      waitUntilEditorDefine(model, app);
+    }, 100);
+  } else {
     var text = window.currentEditor.getValue();
     app.ports.from_monaco.send(text);
-  } else {
-    app.ports.from_monaco.send("");
-    console.log("window.currentEditor is undefined");
   }
+}
+
+export default (model, app) => {
+  waitUntilEditorDefine(model, app);
 };
