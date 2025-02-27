d50789d06152cf5f6a813c9d2ba179aedf10fe97
Worawut Boontan <worawut@theionweb.com>
2025-02-27 13:48:36 +0700

    Refactor Vite configuration and update package name; remove unused dependencies and add new ones

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 // import { plugin } from "vite-plugin-elm";
 
-import monacoEditorPlugin from "vite-plugin-monaco-editor";
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react(),
-    monacoEditorPlugin({}),
     legacy({
       targets: ["defaults", "not IE 11"],
     }),
